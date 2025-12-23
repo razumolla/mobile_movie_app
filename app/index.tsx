3be97be2afb3df2e92e8ddc925ac32d7bc169875
razumolla <razumolla75@gmail.com>
2025-12-23 07:04:44 +0600

    color theme setup

--- app/index.tsx
+++ app/index.tsx
@@ -2,9 +2,14 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
+    <View className="flex-1 items-center justify-center">
       <Text className="text-xl font-bold text-accent">
         Welcome to Nativewind!
+      </Text>
+
+      <Text className="text-2xl font-bold text-light-300">
+        {" "}
+        Welcome to Sohel
       </Text>
     </View>
   );
